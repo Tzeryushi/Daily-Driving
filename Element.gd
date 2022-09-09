557195890e5a0db85eeeb0175f824c131608b8f5
Rhenn Handel <rwhandel@gmail.com>
2022-09-08 23:36:22 -0400

    working deletions
    these will have to follow linking patterns to delete elements in the daily and completed pools, I think, maybe

--- Element.gd
+++ Element.gd
@@ -2,5 +2,14 @@ extends VBoxContainer
 
 onready var element_title = $Title
 
+signal destroy
+
 func set_title(title:String) -> void:
 	element_title.text = title
+
+func destroy() -> void:
+	emit_signal("destroy")
+
+
+func _on_Delete_pressed():
+	destroy() # Replace with function body.
--- ElementContainer.gd
+++ ElementContainer.gd
@@ -5,15 +5,14 @@ onready var pool = $Pool/VBoxContainer
 
 export var new_element : PackedScene
 
-var element_array = []
-
 func _create_new_element(title:String) -> void:
 	var temp_element = new_element.instance()
 	pool.add_child(temp_element)
-	element_array.append(temp_element)
 	temp_element.set_title(title)
+	temp_element.connect("destroy", self, "_on_element_delete", [temp_element])
 	
-
 func _on_NewElement_pressed():
 	_create_new_element(input_field.get_text())
-	print("call")
+
+func _on_element_delete(element) -> void:
+	element.queue_free()
