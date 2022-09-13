ad047f5c2efb3ebabfebcdc69a73f540822d7c33
Rhenn Handel <rwhandel@gmail.com>
2022-09-12 23:46:08 -0400

    implement discrete cumulative density

--- DailyTaskContainer.gd
+++ DailyTaskContainer.gd
@@ -6,9 +6,10 @@ func _ready() -> void:
 	element_container = get_node(element_container)
 #get list of nodes within the pool, select amount based on priority
 func _on_NewDay_pressed():
-	select_index()
+	var chosen = element_container.get_child(select_index())
+	
 
-func select_index() -> void:
+func select_index() -> int:
 	var weight_array = []
 	var full_weight = 0.0
 	#add exclusions to avoid duplicates
@@ -21,4 +22,11 @@ func select_index() -> void:
 	prefix_array.append(weight_array[0])
 	for index in range(1,weight_array.size()):
 		prefix_array.append(prefix_array[index-1]+weight_array[index])
-	full_weight = prefix_array.back()
+	randomize()
+	var random = rand_range(0.0, prefix_array.back())
+	var choose_index = 0
+	for i in range(0, prefix_array.size()):
+		if (prefix_array[i]>random):
+			choose_index = i
+			break
+	return choose_index
